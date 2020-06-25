fe9d3e1187f0bb0dbff94baafc6fa9207a50c8ce
Advait <advaitkukade@gmail.com>
2020-06-25 19:59:31 +0530

    Fixed spelling mistakes, indentation.Added comments.Added some print code to print some simple text

--- handler.py
+++ handler.py
@@ -4,14 +4,13 @@ import numpy as np
 def main(event, context):
 
     a = np.arange(15).reshape(3, 5)
-
-
     print("Your numpy array:")
-
     print(a)
-
-
 
 if __name__ == "__main__":
 
     main('', '')
+
+    print("Added a line")
+    print("Some New Data Here")
+    print("Wassup, how are you")
