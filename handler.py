f5ba908ef1c59fb6dfc5c2f74c0ecd03492c81eb
Advait <advaitkukade@gmail.com>
2020-06-26 20:12:10 +0530

    Adding some text messages to check ci-cd

--- handler.py
+++ handler.py
@@ -8,3 +8,7 @@ def main(event, context):
     r = requests.get("http://www.google.com")
 
     print(r.status_code)
+
+    print("Hello, if you see this, this means that you are done with ci-cd")
+    print("Congratulations !")
+    print("You did it")
